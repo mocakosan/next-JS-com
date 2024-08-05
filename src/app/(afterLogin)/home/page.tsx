fee509ef200e2d5f310af42ab9f700f1daa9a8ed
JangsanHighDev <mocakosan96@highdev.pro>
2024-08-06 02:04:38 +0900

    Add : post in home

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -2,13 +2,14 @@ import style from "./home.module.css";
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
+import Post from "@/app/(afterLogin)/_component/Post";
 
 export default function Home() {
   return (
     <main className={style.main}>
       <TabProvider>
         <Tab />
-        {/* <PostForm />
+        <PostForm />
         <Post />
         <Post />
         <Post />
@@ -20,7 +21,7 @@ export default function Home() {
         <Post />
         <Post />
         <Post />
-        <Post /> */}
+        <Post />
       </TabProvider>
     </main>
   );
