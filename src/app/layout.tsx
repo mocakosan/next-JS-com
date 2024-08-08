eecd0a1d66cb2141031a6c0f617734f5550b3cb5
JangsanHighDev <mocakosan96@highdev.pro>
2024-08-08 14:49:05 +0900

    Add : login with next-auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { MSWComponent } from "./_component/MSWComponent";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,10 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MSWComponent />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+//페이지 접근권한 해주는곳
+import { auth } from "./auth";
+import { NextResponse } from "next/server";
+
+export async function middleware() {
+  const session = await auth();
+  if (!session) {
+    return NextResponse.redirect("http://localhost:3000/i/flow/login");
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/compose/tweet", "/home", "/explore", "/messages", "/search"],
+};
