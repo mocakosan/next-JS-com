3fee831f50d7c8ddc9df087a71a387e943ad1ec1
JangsanHighDev <mocakosan96@highdev.pro>
2024-08-08 18:19:50 +0900

    feat :Different screens depending on when you log in and log out

--- src/app/(afterLogin)/_component/LogOutButton.tsx
+++ src/app/(afterLogin)/_component/LogOutButton.tsx
@@ -1,25 +1,31 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import style from "./logoutButton.module.css";
+import { signOut, useSession } from "next-auth/react";
 
 export default function LogoutButton() {
-  const me = {
-    // 임시로 내 정보 있는것처럼
-    id: "mocakosan",
-    nickname: "mocakosan",
-    image: "/jsLogo.png",
+  const router = useRouter();
+  const { data: me } = useSession();
+
+  const onLogout = () => {
+    signOut({ redirect: false }).then(() => {
+      router.replace("/");
+    });
   };
 
-  const onLogout = () => {};
+  if (!me?.user) {
+    return null;
+  }
 
   return (
     <button className={style.logOutButton} onClick={onLogout}>
       <div className={style.logOutUserImage}>
-        <img src={me.image} alt={me.id} />
+        <img src={me.user?.image as string} alt={me.user?.email as string} />
       </div>
       <div className={style.logOutUserName}>
-        <div>{me.nickname}</div>
-        <div>@{me.id}</div>
+        <div>{me.user?.name}</div>
+        <div>@{me.user?.email}</div>
       </div>
     </button>
   );
