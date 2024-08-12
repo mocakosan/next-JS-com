3b03be0d7578a3ad5051241263fabb090647af80
JangsanHighDev <mocakosan96@highdev.pro>
2024-08-12 21:48:11 +0900

    feat : optimistic update in heart

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -18,18 +18,26 @@ type Props = {
 export default function Post({ noImage, post }: Props) {
   const target = post;
 
+  const stopPropagation = (e) => {
+    e.stopPropagation();
+  };
+
   return (
     <PostArticle post={target}>
       <div className={style.postWrapper}>
         <div className={style.postUserSection}>
-          <Link href={`/${target.User.id}`} className={style.postUserImage}>
+          <Link
+            href={`/${target.User.id}`}
+            className={style.postUserImage}
+            onClick={stopPropagation}
+          >
             <img src={target.User.image} alt={target.User.nickname} />
             <div className={style.postShade} />
           </Link>
         </div>
         <div className={style.postBody}>
           <div className={style.postMeta}>
-            <Link href={`/${target.User.id}`}>
+            <Link href={`/${target.User.id}`} onClick={stopPropagation}>
               <span className={style.postUserName}>{target.User.nickname}</span>
               &nbsp;
               <span className={style.postUserId}>@{target.User.id}</span>
@@ -45,7 +53,7 @@ export default function Post({ noImage, post }: Props) {
               <PostImages post={target} />
             </div>
           )}
-          <ActionButtons />
+          <ActionButtons post={post} />
         </div>
       </div>
     </PostArticle>
--- src/app/(afterLogin)/search/_lib/getSearchResult.ts
+++ src/app/(afterLogin)/search/_lib/getSearchResult.ts
@@ -6,14 +6,16 @@ export const getSearchResult: QueryFunction<
   [_1: string, _2: string, searchParams: { q: string; pf?: string; f?: string }]
 > = async ({ queryKey }) => {
   const [_1, _2, searchParams] = queryKey;
+  const urlSearchParams = new URLSearchParams(searchParams);
   const res = await fetch(
-    `http://localhost:9090/api/search/${
-      searchParams.q
-    }?${searchParams.toString()}`,
+    `${
+      process.env.NEXT_PUBLIC_BASE_URL
+    }/api/posts?${urlSearchParams.toString()}`,
     {
       next: {
         tags: ["posts", "search", searchParams.q],
       },
+      credentials: "include",
       cache: "no-store",
     }
   );
